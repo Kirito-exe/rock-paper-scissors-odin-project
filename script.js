28da6866bf055fff82563bd94db359cf0f4ada8a
Kirito-exe <mannuprakash935@gmail.com>
2026-03-14 07:05:42 +0530

    defined the computer choice by math module and defined rule: 0:rock 1:paper 2or3:scissors

---
+++ script.js
@@ -0,0 +1,6 @@
+function getComputerChoice(){
+    const random = Math.floor(Math.random()*3)
+    if (random===0) return "rock";
+    else if (random===1) return "paper";
+    else return "scissors"
+}
